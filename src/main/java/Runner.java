58288c9228cfefbba97c2f664812fc632e16185e
Yahor Viarenich <gluks0575@gmail.com>
2018-12-11 12:09:49 +0100

    Ending runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,22 +1,30 @@
 import validators.PLIDvalidator;
 import validators.Pesel_validate;
-
 import java.util.Scanner;
-
 public class Runner {
     public static void main(String[] args) {
-//        PLIDvalidator pliDvalidator =new PLIDvalidator();
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Type your ID number");
-//        String idNumb = scanner.nextLine();
-//        boolean idReaz =  pliDvalidator.validate(idNumb);
-//        System.out.println(idReaz);
+                //Validate ID
+        PLIDvalidator pliDvalidator =new PLIDvalidator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type your ID number");
+        String idNumb = scanner.nextLine();
 
+        while (!pliDvalidator.validate(idNumb)){
+            System.out.println("Uncorrect ID");
+            idNumb = scanner.nextLine();
+        }
+        boolean idReaz =  pliDvalidator.validate(idNumb);
+        System.out.println(idReaz);
+
+                //Validate PESEL
         Pesel_validate pesel_validate = new Pesel_validate();
-        System.out.println( pesel_validate.validate("00130090766"));
+        System.out.println("Type your PESEL");
+        String pesel = scanner.nextLine();
 
-
-
-
+        while (!pesel_validate.validate(pesel)){
+            System.out.println("Uncorrect Pesel try again");
+             pesel = scanner.nextLine();
+        }
+        System.out.println( pesel_validate.validate(pesel));
     }
 }
